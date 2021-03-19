895a4af2de460702d33c6188039c069bcb4e269a
Róisín Grannell <r.grannell2@gmail.com>
2021-03-19 18:28:59 +0000

    initial commit

---
+++ .eleventy.js
@@ -0,0 +1,17 @@
+
+const pluginRss = require("@11ty/eleventy-plugin-rss")
+
+let markdownIt = require("markdown-it")
+let markdownItKatex = require("markdown-it-katex")
+let options = {
+  html: true,
+  breaks: false,
+  linkify: true
+}
+
+let markdownLib = markdownIt(options).use(markdownItKatex)
+
+module.exports = function(eleventyConfig) {
+  eleventyConfig.addPlugin(pluginRss)
+  eleventyConfig.setLibrary("md", markdownLib)
+}
