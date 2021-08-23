b6dca8155af425a6f04d8cc188cf5100a5d9f592
Róisín Grannell <r.grannell2@gmail.com>
2021-08-23 23:20:17 +0100

    added katex

--- .eleventy.js
+++ .eleventy.js
@@ -1,26 +1,25 @@
 
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight")
 const pluginRss = require("@11ty/eleventy-plugin-rss")
+const markdownItKaTeX = require('markdown-it-katex');
 
 let markdownIt = require("markdown-it")
-const mathjaxPlugin = require("eleventy-plugin-mathjax");
 let options = {
   html: true,
   breaks: false,
   linkify: true
 }
 
-let markdownLib = markdownIt(options)
 
 module.exports = function(eleventyConfig) {
+  eleventyConfig.addPlugin(syntaxHighlight)
+  eleventyConfig.setLibrary("md", markdownIt({ html: true }).use(markdownItKaTeX))
+
   eleventyConfig.addLiquidFilter("dateToRfc3339", pluginRss.dateRfc3339)
   eleventyConfig.addPlugin(pluginRss)
-  eleventyConfig.addPlugin(syntaxHighlight)
   eleventyConfig.addPassthroughCopy("css")
   eleventyConfig.addPassthroughCopy("manifest.webmanifest")
   eleventyConfig.addPassthroughCopy("sw.js")
   eleventyConfig.addPassthroughCopy("images")
-  eleventyConfig.addPlugin(mathjaxPlugin);
 
-  eleventyConfig.setLibrary("md", markdownLib)
 }
